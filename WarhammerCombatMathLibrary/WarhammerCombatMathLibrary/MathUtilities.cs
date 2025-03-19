8302b68f0d2c7b51d988f0063b62f5cbedb40f11
Michael <michaellogan7860@gmail.com>
2025-03-19 09:39:51 -0500

    Fixed naming for folders, paths, and namespaces.

---
+++ WarhammerCombatMathLibrary/WarhammerCombatMathLibrary/MathUtilities.cs
@@ -0,0 +1,33 @@
+﻿namespace WarhammerCombatMathLibrary
+{
+    /// <summary>
+    /// A static helper class that provides basic math functions.
+    /// </summary>
+    public static class MathUtilities
+    {
+        #region Public Methods
+
+        /// <summary>
+        /// Calculates the factorial of a non-negative integer.
+        /// Factorials are denoted by the syntax "n!".
+        /// </summary>
+        /// <param name="number">
+        /// The integer value to perform the factorial calculation on. 
+        /// The method will use the absolute value of the integer, in case the user passes in a negative value.
+        /// </param>
+        /// <returns>A double containing the factorial of the passed in value.</returns>
+        public static double Factorial(int number)
+        {
+            double result = 1;
+
+            for (int i = 1; i <= Math.Abs(number); i++)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+
+        #endregion
+    }
+}
