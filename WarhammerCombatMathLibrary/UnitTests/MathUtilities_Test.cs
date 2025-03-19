71c7aea3d7b5c982215d09967feff2b0233023de
Michael <michaellogan7860@gmail.com>
2025-03-19 14:42:36 -0500

    Added test methods for factorial method.

---
+++ WarhammerCombatMathLibrary/UnitTests/MathUtilities_Test.cs
@@ -0,0 +1,56 @@
+﻿using WarhammerCombatMathLibrary;
+
+namespace UnitTests
+{
+    /// <summary>
+    /// A test class for the MathUtilities class library.
+    /// </summary>
+    [TestClass]
+    public sealed class MathUtilities_Test
+    {
+        /// <summary>
+        /// Tests that the Factorial method correctly computes the value of 3!.
+        /// </summary>
+        [TestMethod]
+        public void Factorial_TestParameter3()
+        {
+            Assert.AreEqual<int>(6, MathUtilities.Factorial(3));
+        }
+
+        /// <summary>
+        /// Tests that the Factorial method correctly computes the value of 5!.
+        /// </summary>
+        [TestMethod]
+        public void Factorial_TestParameter5()
+        {
+            Assert.AreEqual<int>(120, MathUtilities.Factorial(5));
+        }
+
+        /// <summary>
+        /// Tests that the Factorial method correctly computes the value of 9!.
+        /// </summary>
+        [TestMethod]
+        public void Factorial_TestParameter9()
+        {
+            Assert.AreEqual<int>(362880, MathUtilities.Factorial(9));
+        }
+
+        /// <summary>
+        /// Tests that the Factorial method correctly handles a parameter of 0.
+        /// </summary>
+        [TestMethod]
+        public void Factorial_TestParameter0()
+        {
+            Assert.AreEqual<int>(1, MathUtilities.Factorial(0));
+        }
+
+        /// <summary>
+        /// Tests that the Factorial method throws an error when a negative parameter is given.
+        /// </summary>
+        [TestMethod]
+        public void Factorial_TestNegativeParameter()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => MathUtilities.Factorial(-1));
+        }
+    }
+}
--- WarhammerCombatMathLibrary/WarhammerCombatMathLibrary/MathUtilities.cs
+++ WarhammerCombatMathLibrary/WarhammerCombatMathLibrary/MathUtilities.cs
@@ -8,17 +8,30 @@
         #region Public Methods
 
         /// <summary>
-        /// Calculates the factorial of a non-negative integer.
+        /// Calculates the factorial of a positive integer.
         /// Factorials are denoted by the syntax "n!".
         /// </summary>
         /// <param name="number">
         /// The integer value to perform the factorial calculation on. 
         /// The method will use the absolute value of the integer, in case the user passes in a negative value.
         /// </param>
-        /// <returns>A double containing the factorial of the passed in value.</returns>
-        public static double Factorial(int number)
+        /// <returns>An int containing the factorial of the passed in value.</returns>
+        public static int Factorial(int number)
         {
-            double result = 1;
+            // Throw error if given a negative number
+            if (number < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(number), $"Factorial function is not defined for negative numbers.");
+            }
+
+            // Return 1 if given 0
+            if (number == 0)
+            {
+                return 1;
+            }
+
+            // Multiply together all integers from 1 to the passed in number.
+            int result = 1;
 
             for (int i = 1; i <= Math.Abs(number); i++)
             {
