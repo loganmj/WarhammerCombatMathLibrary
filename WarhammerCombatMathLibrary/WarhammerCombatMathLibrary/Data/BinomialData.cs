68cfddbf1a893222f600b49349895b6de6e7b536
Michael <michaellogan7860@gmail.com>
2025-03-25 08:34:05 -0500

    Added BinomialData object.

---
+++ WarhammerCombatMathLibrary/WarhammerCombatMathLibrary/Data/BinomialData.cs
@@ -0,0 +1,22 @@
+﻿namespace WarhammerCombatMathLibrary.Data
+{
+    /// <summary>
+    /// Data object for binomial data.
+    /// </summary>
+    public class BinomialData
+    {
+        #region Properties
+
+        /// <summary>
+        /// The number of successful trials.
+        /// </summary>
+        public int Successes { get; set; }
+
+        /// <summary>
+        /// The probability of getting the number of successful trials.
+        /// </summary>
+        public double Probability { get; set; }
+
+        #endregion
+    }
+}
